01d9981b1ff1b9d09603eb0709c20a485902fe8a
song <1649996092@qq.com>
2020-05-04 09:52:51 +0800

    '模板渲染'

--- application.py
+++ application.py
@@ -4,9 +4,12 @@ import os
 
 
 class Applciation(Flask):
-    def __init__(self,import_name,template_folder=None,root_path=None):
-        super(Applciation, self).__init__(import_name,template_folder=template_folder,root_path=root_path)
+    def __init__(self,import_name,template_folder=None,root_path=None,static_folder=None):
+        super(Applciation, self).__init__(import_name,template_folder=template_folder,root_path=root_path,static_folder=static_folder)
+
+app = Applciation(__name__,template_folder=os.getcwd()+'/web/templates/',root_path=os.getcwd(),static_folder=os.getcwd()+'web/static/')
+manager = Manager(app)
 
 
-app = Applciation(__name__,template_folder=os.getcwd()+'/web/templates/',root_path=os.getcwd())
-manager = Manager(app) 
+from common.libs.UrlManager import UrlManager
+app.add_template_global(UrlManager.buildStaticUrl,'buildStaticUrl') 
