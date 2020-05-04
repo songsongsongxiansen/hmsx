44609f8635b9707a317962963a2083357b0abde0
song <1649996092@qq.com>
2020-05-04 12:18:51 +0800

    'user模板结束'

--- common/libs/user/UserService.py
+++ common/libs/user/UserService.py
@@ -16,3 +16,13 @@ class UserService():
         str = "%s-%s-%s-%s"%(user_info.uid,user_info.login_name,user_info.login_pwd,user_info.login_salt)
         m.update(str.encode("utf-8"))
         return m.hexdigest()
+    
+    
+    # 生成16位的字符串，包含字母和数字
+    # string.ascii_letters  所有的大小写字母
+    # string.digits 0-9 数字
+    @staticmethod
+    def generateSalt(length=16):
+        keyList = [ random.choice(( string.ascii_letters + string.digits )) for i in range(length) ]
+
+        return ("".join(keyList)) 
--- web/controllers/user/User.py
+++ web/controllers/user/User.py
@@ -67,7 +67,7 @@ def edit():
     # POST请求
     resp = {
         'code':200,
-        'msg':'登录成功',
+        'msg':'编辑成功',
         'data':{}
     }
 
@@ -92,6 +92,47 @@ def edit():
     db.session.commit()
     return jsonify(resp)
 
-@router_user.route("/reset-pwd")
+@router_user.route("/reset-pwd",methods=['GET','POST'])
 def resetPwd():
-    return ops_render("user/reset_pwd.html")
+    if request.method == "GET":
+        return ops_render("user/reset_pwd.html")
+    # POST请求
+    resp = {
+        'code':200,
+        'msg':'重置密码成功',
+        'data':{}
+    }
+
+    req = request.values
+    old_password = req['old_password'] if 'old_password' in req else ''
+    new_password = req['new_password'] if 'new_password' in req else ''
+
+    if old_password is None or len(old_password) < 6:
+        resp['code'] = -1
+        resp['msg'] = "请输入符合规范的旧密码"
+        return jsonify(resp)
+    if new_password is None or len(new_password) < 6:
+        resp['code'] = -1
+        resp['msg'] = "请输入符合规范的新密码"
+        return jsonify(resp)
+
+    if old_password == new_password:
+        resp['code'] = -1
+        resp['msg'] = "新密码和旧密码不能相同"
+        return jsonify(resp)
+
+    user_info = g.current_user
+    #演示账号的保护
+    # if user_info.uid == 1:
+    #     pass
+
+    user_info.login_pwd = UserService.generatePwd(new_password,user_info.login_salt)
+
+    db.session.add(user_info)
+    db.session.commit()
+
+    # 修改cookie中的旧用户信息
+    response = make_response(json.dumps(resp))
+    # Cookie中存入的信息是user_info.uid,user_info
+    response.set_cookie(app.config['AUTH_COOKIE_NAME'],"%s@%s"%(UserService.generateAuthCode(user_info),user_info.uid),60*60*24*15)
+    return response 
